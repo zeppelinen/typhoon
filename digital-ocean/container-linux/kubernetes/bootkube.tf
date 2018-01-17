d85aad1be290ba2e7097152403ca0fb7ae5c01e3
Alexander Naydenko <xn@u-c.su>
2018-01-17 08:02:46 +0300

    typo

--- digital-ocean/container-linux/kubernetes/bootkube.tf
+++ digital-ocean/container-linux/kubernetes/bootkube.tf
@@ -1,6 +1,6 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/zeppelinen/terraform-render-bootkube.git?ref=devel"
+  source = "git::https://github.com/zeppelinen/terraform-render-bootkube.git?ref=develop"
 
   cluster_name          = "${var.cluster_name}"
   api_servers           = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
@@ -11,4 +11,4 @@ module "bootkube" {
   pod_cidr              = "${var.pod_cidr}"
   service_cidr          = "${var.service_cidr}"
   cluster_domain_suffix = "${var.cluster_domain_suffix}"
-}
+
